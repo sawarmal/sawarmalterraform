91c1eb3127aeacc0f949b413e24030ae1455050e
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-28 02:33:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 resource "google_storage_bucket" "bucket" {
   name = "test-bucket-random-001122"
+  service_account    = var.service_account
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-random-001123"
+  service_account    = var.service_account
 }
