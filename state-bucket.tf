1a86dd4f4179f6be9db46db4cfd6187e78ce68ac
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-28 13:00:35 +0530

    Update state-bucket.tf

--- state-bucket.tf
+++ state-bucket.tf
@@ -1,7 +1,8 @@
 resource "google_storage_bucket" "terra-state-buck" {
   force_destroy = true
   service_account = var.service_account
-  location      = var.location
+  location      = var.region
+  zones = var.zones
   storage_class = "STANDARD"
   prefix      = "terraform/state"
   versioning {
