c654cdc81deef17249047516fa9d5436e24a8360
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-28 10:15:04 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "bucket" {
-  name = "test-bucket-random-001122"
-  service_account    = var.service_account
-}
-
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-001123"
-  service_account    = var.service_account
-}
