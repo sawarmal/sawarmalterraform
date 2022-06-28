64d29b038e05f161c58becd0f367cdbbc72ab955
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-28 16:50:51 +0530

    Update state-bucket.tf

--- state-bucket.tf
+++ state-bucket.tf
@@ -1,11 +1,9 @@
-resource "google_storage_bucket" "prashant-terra-buck" {
+resource "google_storage_bucket"  {
+  name = "prashant-terra-buck"
   force_destroy = true
-  credentials = var.credentials
-  service_account = var.service_account
+  creds = var.creds
   location      = var.region
-  zones = var.zones
   storage_class = "STANDARD"
-  prefix      = "terraform/state"
   versioning {
     enabled = true
   }
