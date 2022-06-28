7404a6c9bd78c1c874d5e95ddd77a1d3d62a439e
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-28 16:53:49 +0530

    Update state-bucket.tf

--- state-bucket.tf
+++ state-bucket.tf
@@ -1,7 +1,6 @@
 resource "google_storage_bucket" "prashant-terra-buck"  {
   name = "prashant-terra-buck"
   force_destroy = true
-  creds = var.creds
   location      = var.region
   storage_class = "STANDARD"
   versioning {
