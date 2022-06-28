5985a34f8ae8fe55e8abf45de7842796c3a3dd7c
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-28 10:20:24 +0530

    Update state-bucket.tf

--- state-bucket.tf
+++ state-bucket.tf
@@ -1,8 +1,9 @@
-resource "google_storage_bucket" "default" {
+resource "google_storage_bucket" "terra-state-buck" {
   name          = "bucket-tfstate"
   force_destroy = false
   location      = "us-central1"
   storage_class = "STANDARD"
+  prefix      = "terraform/state"
   versioning {
     enabled = true
   }
