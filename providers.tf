e08b3a84f3c9fdae4549efc0990b4e04793b9714
psk24091988 <108300678+psk24091988@users.noreply.github.com>
2022-06-28 16:41:55 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,6 @@
 provider "google" {
   project = var.project_id
+  credentials = var.creds
   region  = var.region
   service_account = var.service_account
 }
@@ -13,6 +14,7 @@ terraform {
 
 provider "google-beta" {
   service_account = var.service_account
+  credentials = var.creds
   project     = var.project_id
   region      = var.region
 }
