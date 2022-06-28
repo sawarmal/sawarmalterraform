23ee959f021318bcdbb193d844304868dfd51d80
sawarmal <91613206+sawarmal@users.noreply.github.com>
2022-06-29 01:17:54 +0530

    Delete state-bucket.tf

--- state-bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "sawarmalbucket"  {
-  name = "sawarmalbucket"
-  force_destroy = true
-  location      = var.region
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
