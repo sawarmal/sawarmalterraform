5a5ccc92c5a922e6edeb761b054a7a9a7a40687d
sawarmal <91613206+sawarmal@users.noreply.github.com>
2022-06-29 02:01:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+module "enabled_google_apis" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "~> 11.3"
+
+  project_id                  = var.project_id
+  disable_services_on_destroy = false
+
+  activate_apis = [
+    "compute.googleapis.com",
+    "container.googleapis.com",
+    "gkehub.googleapis.com",
+    "anthosconfigmanagement.googleapis.com"
+  ]
+}
 module "gke" {
   source                     = "terraform-google-modules/kubernetes-engine/google"
   version                    = "~> 16.0"
